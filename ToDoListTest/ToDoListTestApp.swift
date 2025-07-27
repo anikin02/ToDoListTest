910185e6569f2466e457a9ffa08e168cd5081091
Danil Anikin <mart4r.clearlist@gmail.com>
2025-07-27 17:53:39 +1000

    Initial Commit

---
+++ ToDoListTest/ToDoListTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  ToDoListTestApp.swift
+//  ToDoListTest
+//
+//  Created by Данил Аникин on 27.07.2025.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
