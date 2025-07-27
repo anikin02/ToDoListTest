0bbe50325ad7442f7838b2ace6c844dbd143638c
Danil Anikin <mart4r.clearlist@gmail.com>
2025-07-27 19:01:10 +1000

    Added FooterView

---
+++ ToDoListTest/View/ListView/ItemListView.swift
@@ -0,0 +1,37 @@
+//
+//  ItemListView.swift
+//  ToDoListTest
+//
+//  Created by Данил Аникин on 27.07.2025.
+//
+
+import SwiftUI
+
+struct ItemListView: View {
+  let item: TodoItem
+  var body: some View {
+    HStack(alignment: .top) {
+      HStack {
+        Image(systemName: item.completed ? "checkmark.circle" : "circle")
+          .resizable()
+          .frame(width: 24, height: 24)
+          .foregroundStyle(item.completed ? .YELLOW : .GRAY)
+      }
+      VStack(alignment: .leading) {
+        Text(item.todo)
+          .font(.system(size: 22, weight: .medium))
+          .foregroundStyle(item.completed ? .GRAY : .TEXTCOLOR)
+          .lineLimit(1)
+          .strikethrough(item.completed, color: .GRAY)
+        Text(item.description)
+          .lineLimit(2)
+          .font(.system(size: 16, weight: .regular))
+          .foregroundStyle(item.completed ? .GRAY : .TEXTCOLOR)
+        Text(item.date)
+          .foregroundStyle(.GRAY)
+          .font(.system(size: 16, weight: .regular))
+      }
+    }
+    .frame(maxWidth: .infinity, alignment: .leading)
+  }
+}
--- ToDoListTest/View/ListView/ListView.swift
+++ ToDoListTest/View/ListView/ListView.swift
@@ -14,29 +14,64 @@ struct ListView: View {
   var body: some View {
     VStack {
       ScrollView {
-        ItemListView()
-        ItemListView()
-        ItemListView()
-        ItemListView()
+        ItemListView(item: TodoItem(todo: "Название", description: "описвание задачи", completed: false))
+        ItemListView(item: TodoItem(todo: "Название Название Название Название Название", description: "описвание задачи", completed: false))
+        ItemListView(item: TodoItem(todo: "Название", description: "описвание задачи", completed: true))
+        ItemListView(item: TodoItem(todo: "Название", description: "описвание задачи", completed: false))
+        ItemListView(item: TodoItem(todo: "Название", description: "описваниеопис ваниеоп исвание описваниео писваниеописвание описвание задачи ", completed: true))
+        ItemListView(item: TodoItem(todo: "Название", description: "описваниеопис ваниеоп исвание описваниео писваниеописвание описвание задачи ", completed: true))
+        ItemListView(item: TodoItem(todo: "Название", description: "описваниеопис ваниеоп исвание описваниео писваниеописвание описвание задачи ", completed: true))
+        ItemListView(item: TodoItem(todo: "Название", description: "описваниеопис ваниеоп исвание описваниео писваниеописвание описвание задачи ", completed: true))
+        ItemListView(item: TodoItem(todo: "Название", description: "описваниеопис ваниеоп исвание описваниео писваниеописвание описвание задачи ", completed: true))
+        ItemListView(item: TodoItem(todo: "Название", description: "описваниеопис ваниеоп исвание описваниео писваниеописвание описвание задачи ", completed: true))
       }
       .frame(maxWidth: .infinity)
+      .padding(.horizontal, 20)
+      .scrollIndicators(.hidden)
       
-      HStack {
-        
-      }
+      FooterView(count: 7)
     }
   }
 }
 
-struct ItemListView: View {
+struct FooterView: View {
+  let count: Int
   var body: some View {
-    HStack {
+    ZStack {
+      Text(getStringCount())
+        .font(.system(size: 18, weight: .regular))
+        .foregroundStyle(.white)
+      
       HStack {
+        Spacer()
         
+        Button {
+        } label: {
+          Image(systemName: "square.and.pencil")
+            .resizable()
+            .frame(width: 28, height: 28)
+            .foregroundStyle(.YELLOW)
+        }
       }
-      VStack {
-        
-      }
+      .padding(.horizontal, 20)
+    }
+    .frame(maxWidth: .infinity, minHeight: 50)
+    .background(Color.black.opacity(0.5))
+  }
+  
+  func getStringCount() -> String {
+    switch count {
+    case 1:
+      return "1 Задача"
+    case 2..<5:
+      return "\(count) Задачи"
+    default:
+      return "\(count) Задач"
     }
   }
+}
+
+
+#Preview {
+  ContentView()
 }
